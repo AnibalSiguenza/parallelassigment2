dce49694a6fc52971f2806d52f857f093e0935e9
Anibal <anibal.siguenza1@gmail.com>
2017-05-23 10:52:31 +0200

    First commit

---
+++ student/histogram_par.c
@@ -0,0 +1,14 @@
+#include "histogram.h"
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include "names.h"
+
+void get_histogram(char *buffer, int* histogram, int num_threads)
+{
+	// write your parallel solution here
+}
